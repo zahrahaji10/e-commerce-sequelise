adc6d811080e58b907cd529787372eef7bbb0374
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-04 14:29:12 +0100

    created server and created connection

---
+++ src/server.js
@@ -0,0 +1,35 @@
+// external imports
+const express = require("express");
+
+// internal imports
+const routes = require("./routes");
+const connection = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+const init = async () => {
+  try {
+    // connect to DB
+    await connection.sync({ force: false });
+
+    // server listen on PORT
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
+};
+
+init();
