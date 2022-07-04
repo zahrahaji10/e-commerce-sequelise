1165933d291d4d74008d9213302c17b568010626
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-04 14:39:13 +0100

    amened connection fn to catch error

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 // internal imports
-const routes = require("./routes");
+// const routes = require("./routes");
 const connection = require("./config/connection");
 
 const app = express();
@@ -11,12 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// app.use(routes);
 
 const init = async () => {
   try {
