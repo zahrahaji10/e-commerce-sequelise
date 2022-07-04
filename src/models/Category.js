07c4d702d609642d41f9bafff6d90d00a6160851
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-04 19:49:16 +0100

    refactored category model and defined columns

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+  CategoryName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "category",
+};
+
+Books.init(schema, options);
 
 module.exports = Category;
