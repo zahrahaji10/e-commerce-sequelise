29c43f59d5a7630b5a6afca5fad20ff6239176df
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-04 16:00:27 +0100

    server all connected, reaching endpoints

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 // internal imports
-// const routes = require("./routes");
+const routes = require("./routes");
 const connection = require("./config/connection");
 
 const app = express();
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 const init = async () => {
   try {
