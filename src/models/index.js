8b856487983a07f2c62cc55465cb6201e725dd4b
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-05 16:28:07 +0100

    associations between models done

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// associations between the tables
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.belongsToMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
