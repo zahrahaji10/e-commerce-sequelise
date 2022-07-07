ce342e5f34895288d4e32a766de73c3fb7b05166
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-07 01:12:47 +0100

    tag controller functions working

--- src/controllers/api/tag.js
+++ src/controllers/api/tag.js
@@ -1,33 +1,102 @@
-const { Tag, Product, ProductTag } = require("../../models");
+const { Tag, Product, Category, ProductTag } = require("../../models");
 
 // get all tags fn
-const getAllTags = (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  return res.send("getAllTags");
+const getAllTags = async (req, res) => {
+  try {
+    // find all tags
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ["id", `product_name`, "price", "stock", "category_id"],
+        },
+      ],
+    });
+
+    // return response
+    return res.json({ data: tags, status: 200 });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get tags | ${error.message}`);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 // get one tag fn
-const getTagById = (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  return res.send("getTagById");
+const getTagById = async (req, res) => {
+  try {
+    // get id from req params
+    const tagId = req.params.id;
+
+    // find one tag by id
+    const tag = await Tag.findByPk(tagId, {
+      include: [
+        {
+          model: Product,
+          attributes: [`product_name`, "price", "stock", "category_id"],
+        },
+      ],
+    });
+
+    // return response
+    return res.json({ data: tag, status: 200 });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get tag by id | ${error.message}`);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 // create new tag fn
-const createTag = (req, res) => {
-  // create a new tag
-  return res.send("createTag");
+const createTag = async (req, res) => {
+  try {
+    // create the body for to create tag
+    const { id, tag_name } = req.body;
+
+    //  create a new category using category model
+    const newTag = await Tag.create(req.body);
+
+    // return response
+    return res.json({ data: newTag, status: 200 });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create a tag | ${error.message}`);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 // update tag fn
-const updateTagById = (req, res) => {
-  // update a tag's name by its `id` value
-  return res.send("updateTagById");
+const updateTagById = async (req, res) => {
+  try {
+    // get id from req params
+    const tag = req.params.id;
+
+    //  update tag using category model
+    const updateTag = await Tag.update(req.body, {
+      where: { id: tag },
+    });
+
+    // return response
+    return res.json({ data: updateTag, status: 200 });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to update a tag | ${error.message}`);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 // delete product fn
-const deleteTagById = (req, res) => {
+const deleteTagById = async (req, res) => {
+  try {
+    // get tag by id
+    const tag = req.params.id;
+
+    // delete a tag using tag model
+    const deleteTag = await Tag.destroy({
+      where: { id: tag },
+    });
+    // return response
+    return res.json({ data: deleteTag, status: 200 });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete a tag | ${error.message}`);
+    return res.status(500).json({ error: "Internal server error" });
+  }
   // update a tag's name by its `id` value
   return res.send(" deleteTagById");
 };
